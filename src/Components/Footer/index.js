0d070e28ee443591ac6773fa529b1002cb261ebe
Jared Ryan <jared.michael.ryan@ibm.com>
2019-03-05 22:04:35 -0600

    footer changes

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -9,9 +9,9 @@ class Footer extends Component {
                     <div className="footerGrid">
                         <div className="border1"></div><h3>Connect</h3><div className="border2"></div>
                     </div>
-                    <a href="https://github.com/jaredryan"><i className="fa fa-github"></i></a>
-                    <a href="https://www.linkedin.com/in/jared-m-ryan/"><i className="fa fa-linkedin"></i></a>
-                    <a href="mailto:jryantennis@gmail.com?subject=Hi, Jared (from the Portfolio Website)"><i className="fa fa-envelope"></i></a>
+                    <a href="https://github.com/jaredryan"><i className="fa fa-github" style={{fontSize: "28px", fontWeight: 100}}></i></a>
+                    <a href="https://www.linkedin.com/in/jared-m-ryan/"><i className="fa fa-linkedin" style={{fontSize: "28px", fontWeight: 100}}></i></a>
+                    <a href="mailto:jryantennis@gmail.com?subject=Hi, Jared (from the Portfolio Website)"><i className="fa fa-envelope" style={{fontSize: "28px", fontWeight: 100}}></i></a>
                 </div>
             </footer>
         );
