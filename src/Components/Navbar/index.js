eabb8515655314cabfc6e8e18412b4be99ea977a
Jared Ryan <jared.michael.ryan@ibm.com>
2019-03-24 10:17:17 -0500

    refactored the navbar

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -7,10 +7,10 @@ class Navbar extends Component {
         return(
             <nav>
                 <ul>
-                    <Link to="/"><li>Home</li></Link>
-                    <Link to="/about"><li>About</li></Link>
-                    <Link to="/projects"><li>Projects</li></Link>
-                    <Link to="/resume"><li>Resume</li></Link>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/projects">Projects</Link></li>
+                    <li><Link to="/resume">Resume</Link></li>
                 </ul>
                 <div className="overlay"></div>
             </nav>
