bf60199bc04041bef13fcc6ab4d34a8a9a8171d8
Jared Ryan <jared.michael.ryan@ibm.com>
2019-04-01 13:55:42 -0500

    just need to work on last page

--- src/Sections/WhyMe/index.js
+++ src/Sections/WhyMe/index.js
@@ -2,40 +2,57 @@ import React from 'react'
 import './index.css'
 
 const WhyMe = () => {
-    return (
-        <div className="whyMe" id="whyMe">
-            <div className="banner"></div>
-            <div className="flex">
-                <div className="whyMeText">
-                    <h1>Why Me?</h1>
-                    <ul>
-                        <li>Through my professional experience at IBM since June 2018, I know how to learn quickly on the job and work with a team.</li>
-                        <li>Freelance web development since April 2018 has taught me how to communicate with clients.</li>
-                        <li>While studying at UC Berkeley, I learned the value of hard work, focus, and organization.</li>
-                        <li>I've spent a year gaining experience leading, managing, training, and coaching teams of up to 20 people while volunteering in Brazil.</li>
-                        <li>From these experiences, in addition to developing these skills, I've come to embrace goal-setting, optimism, and humor.</li>
-                    </ul>
-                    <div className="pics">
-                        <div className="whyMePic left">
-                            <img src={require("../../Images/whyMe.jpg")} alt=""/>
-                        </div>
-                        <div className="whyMePic right">
-                            <img src={require("../../Images/whyMeForest.jpg")} alt=""/>
-                        </div>
+    const points = [
+        {
+            text: 'Through my professional experience at IBM since June 2018, I know how to learn quickly on the job and work with a team.',
+            image: 'whyMeIBM'
+        },
+        {
+            text: 'Freelance web development since April 2018 has taught me how to work and communicate with clients.',
+            image: 'whyMeFreelance'
+        },
+        {
+            text: 'While studying at UC Berkeley, I learned the value of hard work, focus, and organization, as well as optimism and humor.',
+            image: 'whyMeBerkeley'
+        },
+        {
+            text: "I've spent a year gaining experience leading, managing, training, and coaching teams of up to 20 people while volunteering in Brazil.",
+            image: 'whyMeBrazil'
+        }
+    ]
+
+    const mappedPoints = points.map((point, index) => {
+        if (index % 2 === 0) {
+            return (
+                <div className="point">
+                    <div className="picContainer">
+                        <div className="pic" id={point.image}></div>
+                    </div>
+                    <div className="text">
+                        <p>{point.text}</p>
                     </div>
                 </div>
-                <div className="contactMe">
-                    <div className="profilePic">
-                        <img src={require("../../Images/profilePic.jpg")} alt=""/>
+            )
+        } else {
+            return (
+                <div className="point">
+                    <div className="text">
+                        <p>{point.text}</p>
                     </div>
-                    <div className="seeMore">
-                        <h1>Contact Me</h1>
-                        <div className="fontAwesomeLogos">
-                            <a href="https://github.com/jaredryan"><i className="fa fa-github" style={{fontSize: "44px", fontWeight: 100}}></i></a>
-                            <a href="https://www.linkedin.com/in/jared-m-ryan/"><i className="fa fa-linkedin" style={{fontSize: "44px", fontWeight: 100}}></i></a>
-                            <a href="mailto:jryantennis@gmail.com?subject=Hi, Jared (from the Portfolio Website)"><i className="fa fa-envelope" style={{fontSize: "44px", fontWeight: 100}}></i></a>
-                        </div>
+                    <div className="picContainer">
+                        <div className="pic" id={point.image}></div>
                     </div>
+                </div>
+            )
+        }
+    })
+
+    return (
+        <div className="whyMe" id="whyMe">
+            <div className="flex">
+                <h1>Why Me?</h1>
+                <div className="points">
+                    {mappedPoints}
                 </div>
             </div>
         </div>
