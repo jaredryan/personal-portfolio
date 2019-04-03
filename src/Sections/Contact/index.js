828261b726cd8971687bbc8b63f9eac02e000d68
Jared Ryan <jared.michael.ryan@ibm.com>
2019-04-03 18:21:37 -0500

    adjusting contact css

--- src/Sections/Contact/index.js
+++ src/Sections/Contact/index.js
@@ -25,9 +25,9 @@ class Contact extends Component {
                         </div>
                         <div className="seeMore">
                             <div className="fontAwesomeLogos">
-                                <a href="mailto:jryantennis@gmail.com?subject=Hi, Jared (from the Portfolio Website)"><i className="fa fa-envelope" style={{fontSize: "32px", fontWeight: 100}}></i></a>
+                                <a href="mailto:jryantennis@gmail.com?subject=Hi, Jared (from the Portfolio Website)" className="left"><i className="fa fa-envelope" style={{fontSize: "32px", fontWeight: 100}}></i></a>
                                 <a href="https://github.com/jaredryan"><i className="fa fa-github" style={{fontSize: "32px", fontWeight: 100}}></i></a>
-                                <a href="https://www.linkedin.com/in/jared-m-ryan/"><i className="fa fa-linkedin" style={{fontSize: "32px", fontWeight: 100}}></i></a>
+                                <a href="https://www.linkedin.com/in/jared-m-ryan/" className="right"><i className="fa fa-linkedin" style={{fontSize: "32px", fontWeight: 100}}></i></a>
                             </div>
                         </div>
                     </div>
