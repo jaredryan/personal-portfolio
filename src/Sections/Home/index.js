12ede6497209a4c6ad904784853349e857ac514e
Jared Ryan <jared.michael.ryan@ibm.com>
2019-04-24 21:54:07 -0500

    new home page image

--- src/Sections/Home/index.js
+++ src/Sections/Home/index.js
@@ -77,9 +77,9 @@ class Home extends Component {
                     <h1>Jared Ryan</h1>
                     <h2>Software and Web Developer</h2>
                     <div className="fontAwesomeLogos">
-                        <a href="mailto:jryantennis@gmail.com?subject=Hi, Jared (from the Portfolio Website)" className="left"><i className="fa fa-envelope" style={{fontSize: "40px", fontWeight: 100}}></i></a>
-                        <a href="https://github.com/jaredryan"><i className="fa fa-github" style={{fontSize: "40px", fontWeight: 100}}></i></a>
-                        <a href="https://www.linkedin.com/in/jared-m-ryan/" className="right"><i className="fa fa-linkedin" style={{fontSize: "40px", fontWeight: 100}}></i></a>
+                        <a href="mailto:jryantennis@gmail.com?subject=Hi, Jared (from the Portfolio Website)" className="left"><i className="fa fa-envelope" style={{fontWeight: 100}}></i></a>
+                        <a href="https://github.com/jaredryan"><i className="fa fa-github" style={{fontWeight: 100}}></i></a>
+                        <a href="https://www.linkedin.com/in/jared-m-ryan/" className="right"><i className="fa fa-linkedin" style={{fontWeight: 100}}></i></a>
                     </div>
                 </div>
                 <div className="homeImg"></div>
