b7990fd3e15084e5ad7cc0a5b1777b25880a5c4e
Jared Ryan <jared.michael.ryan@ibm.com>
2019-05-05 22:26:26 -0500

    trying again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import OnImagesLoaded from 'react-on-images-loaded';
 
 import Navbar from './Components/Navbar'
 import Home from './Sections/Home'
@@ -8,29 +7,30 @@ import Resume from './Sections/Resume'
 import Projects from './Sections/Projects'
 import Contact from './Sections/Contact'
 
-
 class App extends Component {
     constructor() {
         super();
         this.state = {
+            imgCount: 0,
+            imgLoadCount: 0,
             loading: true,
             animation: {},
             loadingDisplay: 'none',
             pagesDisplay: 'none',
-            appStyle: {height: '100vh', width: '100%', position: 'relative'}
-        }
-        this.refs = {
-            navbar: React.createRef(),
-            home: React.createRef(),
-            whyMe: React.createRef(),
-            resume: React.createRef(),
-            projects: React.createRef(),
-            contact: React.createRef()
+            appStyle: {height: '100vh', width: '100%', position: 'relative'},
+            refs: {
+                navbar: React.createRef(),
+                home: React.createRef(),
+                whyMe: React.createRef(),
+                resume: React.createRef(),
+                projects: React.createRef(),
+                contact: React.createRef()
+            }
         }
     }
 
     componentDidMount() {
-        window.addEventListener('load', this.handleLoad)
+        window.addEventListener('load', this.handleLoad);
     }
 
     componentWillUnmount() {
@@ -38,18 +38,20 @@ class App extends Component {
     }
 
     handleLoad = () => {
-        // const self = this;
-        // self.setState({loading: false})
-        // var op = 1;  // initial opacity
-        // var timer = setInterval(function () {
-        //     if (op <= 0.1){
-        //         clearInterval(timer);
-        //         self.setState({loadingDisplay: 'none', pagesDisplay: 'block', appStyle: {}})
-        //     } else {
-        //         self.setState({animation: {opacity: `${op}`, filter: `alpha(opacity=${op * 100})`}})
-        //         op -= op * 0.1;
-        //     }
-        // }, 5);
+        const self = this;
+        setTimeout(() => {
+            self.setState({loading: false})
+            var op = 1;  // initial opacity
+            var timer = setInterval(function () {
+                if (op <= 0.1){
+                    clearInterval(timer);
+                    self.setState({loadingDisplay: 'none', pagesDisplay: 'block', appStyle: {}})
+                } else {
+                    self.setState({animation: {opacity: `${op}`, filter: `alpha(opacity=${op * 100})`}})
+                    op -= op * 0.1;
+                }
+            }, 5);
+        }, 5000)
     }
 
     render() {
@@ -65,17 +67,14 @@ class App extends Component {
                         <h1>Loading...</h1>
                     </div>
                 </div>
-                <OnImagesLoaded
-                    onLoaded={this.handleLoad}
-                    style={{display: this.state.pagesDisplay}}
-                >
-                    <Navbar refs={this.refs}/>
-                    <Home refs={this.refs}/>
-                    <WhyMe refs={this.refs}/>
-                    <Resume refs={this.refs}/>
-                    <Projects refs={this.refs}/>
-                    <Contact refs={this.refs}/>
-                </OnImagesLoaded>
+                <div style={{display: this.state.pagesDisplay}}>
+                    <Navbar refs={this.state.refs}/>
+                    <Home refs={this.state.refs}/>
+                    <WhyMe refs={this.state.refs}/>
+                    <Resume refs={this.state.refs}/>
+                    <Projects refs={this.state.refs}/>
+                    <Contact refs={this.state.refs}/>
+                </div>
             </div>
         )
     }
